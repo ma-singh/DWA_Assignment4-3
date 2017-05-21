2060eda410408723940deef89d66b2b56cec5605
ma-singh <masingh@fullsail.edu>
2017-05-21 03:00:12 -0400

    testing hook and unit test

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('fake test', () => {
+  it('this should work', () => {
+    const variable = 25;
+
+    expect(variable).to.be.a('number');
+  });
+});
